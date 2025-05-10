553c4be460da1a52533c74159112260945167a3d
corrymp <corrympalmer@gmail.com>
2025-05-09 22:55:07 -0700

    linted and formatted :facepalm:

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-    const cart = getLocalStorage('so-cart') || [];
-    cart.push(product);
+  const cart = getLocalStorage('so-cart') || [];
+  cart.push(product);
   setLocalStorage('so-cart', cart);
 }
 // add to cart button event handler
