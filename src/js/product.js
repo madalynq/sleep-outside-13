8bdb6ca099bdeb32cf14c8953c6ee80610a1efd8
madalynq <iwa.maddy@gmail.com>
2025-05-14 17:52:43 -0600

    team activity week2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,11 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage('so-cart') || [];
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// console.log(dataSource.findProductById(productId));
