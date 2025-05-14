cdf255509bba43ab085633582a803a5a0f7b1244
Tyson <32.tpace@gmail.com>
2025-05-14 15:24:38 -0600

    Revert "Add to cart broken"

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,10 @@
 import { setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
-import { getLocalStorage } from './utils.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage('so-cart') || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
+  setLocalStorage('so-cart', product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
