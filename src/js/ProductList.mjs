af9c512c9ac0d232517a5329997720bf3975dbdc
tysonpace777 <32.tpace@gmail.com>
2025-05-22 13:17:24 -0600

    discount indicator finished

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,14 +1,30 @@
 import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
-  return `<li class="product-card">
-            <a href="../product_pages/?product=${product.Id}">
-                <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
-                <h2 class="card__brand">${product.Brand.Name}</h2>
-                <h3 class="card__name">${product.Name}</h3>
-                <p class="product-card__price">${product.FinalPrice}</p>
-            </a>
-        </li>`;
+  const discountPercent = ((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100;
+
+  if(product.FinalPrice.toFixed(2) < product.SuggestedRetailPrice.toFixed(2))
+    return `<li class="product-card">
+              <a href="../product_pages/?product=${product.Id}">
+                  <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+                  <h2 class="card__brand">${product.Brand.Name}</h2>
+                  <h3 class="card__name">${product.Name}</h3>
+                  <p class="product-card__price" id="sale">$${product.FinalPrice.toFixed(2)}</p>
+                  <p>
+                    <span id="discount">$${product.SuggestedRetailPrice.toFixed(2)}</span>
+                    <span class="percent">${discountPercent.toFixed(0)}% off</span>
+                  </p>
+              </a>
+          </li>`;
+  else
+    return `<li class="product-card">
+              <a href="../product_pages/?product=${product.Id}">
+                  <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+                  <h2 class="card__brand">${product.Brand.Name}</h2>
+                  <h3 class="card__name">${product.Name}</h3>
+                  <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
+              </a>
+          </li>`;
 }
 
 export default class ProductList {
