8e6cc954692d556a06ea9fad7bfcd8155189c56f
madalynq <iwa.maddy@gmail.com>
2025-05-21 18:53:06 -0600

    forgot to run lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 import Alert from './Alert.mjs';
-import updateCartCount from './cart.js'
+import updateCartCount from './cart.js';
 
 const productData = new ProductData('tents');
 const productList = new ProductList(
